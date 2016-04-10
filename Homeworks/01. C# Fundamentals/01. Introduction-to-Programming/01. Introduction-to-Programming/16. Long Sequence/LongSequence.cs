e842e201efd04c7482a52f9f46b6fead802c4301
Vasil <vaskyn@gmail.com>
2016-04-10 14:03:55 +0300

    01. Introduction-to-Programming

---
+++ Homeworks/01. C# Fundamentals/01. Introduction-to-Programming/01. Introduction-to-Programming/16. Long Sequence/LongSequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LongSequence
+{
+    class LongSequence
+    {
+        static void Main()
+        {
+            int currentPostiveNumber = 2;
+            int currentNegativeNumber = -3;
+            for (int i = 0; i <= 499; i++)
+            {
+                {
+                    Console.WriteLine(currentPostiveNumber);
+                    Console.WriteLine(currentNegativeNumber);
+                    currentPostiveNumber += 2;
+                    currentNegativeNumber -= 2;
+                }
+            }
+        }
+    }
+}
