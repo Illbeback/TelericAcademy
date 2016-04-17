c8a02fb23c0542ded6032e6f70310f4653caa078
Vasil <vaskyn@gmail.com>
2016-04-17 18:14:31 +0300

    04. Console-In-and-Out

---
+++ Homeworks/01. C# Fundamentals/04. Console-In-and-Out/03. Circle/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Circle
+{
+    static void Main()
+    {
+        double radius = double.Parse(Console.ReadLine());
+        double Pi = Math.PI;
+        double perimeter = (2 * Pi * radius);
+        double area = (Pi * (radius * radius));
+
+        Console.WriteLine("{0:0.00} {1:0.00}", perimeter, area);
+
+    }
+}
---
+++ Homeworks/01. C# Fundamentals/04. Console-In-and-Out/05. Numbers Comparer/NumbersComparer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class NumbersComparer
+{
+    static void Main()
+    {
+        double numberOne = double.Parse(Console.ReadLine());
+        double numberTwo = double.Parse(Console.ReadLine());
+        bool result = numberOne > numberTwo;
+        Console.WriteLine("{0}", result ? numberOne : numberTwo);
+    }
+}
