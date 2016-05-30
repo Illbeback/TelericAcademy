fbd8c03cde39287eb99d6335bd79706100d70a5f
Vasil <vaskyn@gmail.com>
2016-05-30 12:08:16 +0300

    07. Exception Handling homework

---
+++ Homeworks/03. C# Advanced/07. Exception Handling/01. Square root/SquareRoot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            try
+            {
+                double number = double.Parse(input);
+
+                double result = Math.Sqrt(number);
+
+                if (number < 0 || number == 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid number");
+                }
+                else if (number == double.NaN)
+                {
+                    throw new FormatException("Invalid number");
+                }
+                else
+                {
+                    Console.WriteLine("{0:F3}", result);
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ Homeworks/03. C# Advanced/07. Exception Handling/02. Enter numbers/EnterNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class EnterNumers
+{
+    static void Main()
+    {
+        int[] arr = new int[12];
+
+        arr[0] = 1;
+        for (int i = 1; i < arr.Length - 1; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        arr[11] = 100;
+
+        for (int i = 0; i < 9; i++)
+        {
+            if (ReadNumber(arr[i], arr[i + 1]))
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Exception");
+                return;
+            }
+        }
+
+        Console.WriteLine(string.Join(" < ", arr));
+    }
+
+    static bool ReadNumber(int start, int end)
+    {
+        if (start < end)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
